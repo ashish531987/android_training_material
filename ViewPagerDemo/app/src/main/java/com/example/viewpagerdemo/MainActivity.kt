2884b59df83b614f97968808200527c82d87eee8
Ashish Mahamuni <ashish531987@gmail.com>
2020-09-09 10:47:43 +0530

    Tabs added and Layout corrected.

--- ViewPagerDemo/app/src/main/java/com/example/viewpagerdemo/MainActivity.kt
+++ ViewPagerDemo/app/src/main/java/com/example/viewpagerdemo/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 private const val NUM_PAGES = 5
 class MainActivity : AppCompatActivity() {
@@ -18,8 +20,15 @@ class MainActivity : AppCompatActivity() {
 
         viewPager = findViewById(R.id.viewPager)
 
+        val tabLayout = findViewById<TabLayout>(R.id.tabs)
+
         val pagerAdapter = ScreenSlidePagerAdapter(this)
         viewPager.adapter = pagerAdapter
+
+        TabLayoutMediator(tabLayout, viewPager){
+            tab, position -> tab.text = "Tab $position"
+            viewPager.setCurrentItem(position, true)
+        }.attach()
     }
 
     override fun onBackPressed() {
